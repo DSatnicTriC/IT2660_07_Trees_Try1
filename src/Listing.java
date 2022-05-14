5e5c1d9246efc983185072c1171268acbf8b1073
Daniel Satnic <S01031109@acad.tri-c.edu>
2022-05-14 13:17:57 -0400

    completed requirements for exercise 30 of assignment ... working on exercise 31

--- src/Listing.java
+++ src/Listing.java
@@ -3,49 +3,49 @@ public class Listing {
 
 	// start of privates and encapsulated fields
 	private String name; // key field
-	private String idNumber;
-	private String gradePointAverage;
-	
+	private int idNumber;
+	private double gradePointAverage;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getIdNumber() {
+
+	public int getIdNumber() {
 		return idNumber;
 	}
-	public void setIdNumber(String idNumber) {
+
+	public void setIdNumber(int idNumber) {
 		this.idNumber = idNumber;
 	}
-	public String getGradePointAverage() {
+
+	public double getGradePointAverage() {
 		return gradePointAverage;
 	}
-	public void setGradePointAverage(String gradePointAverage) {
+
+	public void setGradePointAverage(double gradePointAverage) {
 		this.gradePointAverage = gradePointAverage;
 	}
-	// end  of privates and encapsulated fields
-	
-	public Listing(String name, String idNumber, String gradePointAverage) {
+	// end of privates and encapsulated fields
+
+	public Listing(String name, int idNumber, double gradePointAverage) {
 		this.name = name;
 		this.idNumber = idNumber;
 		this.gradePointAverage = gradePointAverage;
 	}
-	
+
 	public String toString() {
-		var toString = "Name: "
-				+ this.name
-				+ "\nId: "
-				+ this.idNumber
-				+ "\nGpa: "
-				+ this.gradePointAverage;
+		var toString = "Name: " + this.name + "\nId: " + this.idNumber + "\nGpa: " + this.gradePointAverage;
 		return toString;
 	}
-	
+
 	public Listing deepCopy() {
 		return new Listing(this.name, this.idNumber, this.gradePointAverage);
 	}
-	
+
 	public int compareTo(String targetKey) {
 		return this.name.compareTo(targetKey);
 	}
--- src/TreeRunner.java
+++ src/TreeRunner.java
@@ -1,9 +1,27 @@
-
 public class TreeRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		var listing01 = new Listing("Daniel", 1, 3.5);
+		var listing02 = listing01.deepCopy();
+		listing02.setName(listing02.getName() + "HA");
+		listing02.setIdNumber(listing02.getIdNumber() + 1);
+		listing02.setGradePointAverage(listing02.getGradePointAverage() + 1);
 
+		System.out.println("Listing01");
+		System.out.println(listing01.toString());
+		System.out.println("\n");
+		System.out.println("Listing02");
+		System.out.println(listing02.toString());
+		System.out.println("\n");
+
+		Integer comparison = listing02.compareTo(listing01.getName());
+
+		if (comparison == 0) {
+			System.out.println("The string is equal to the compared string");
+		} else if (comparison < 0) {
+			System.out.println("The string is lexicographically less than the other string");
+		} else if (comparison > 0) {
+			System.out.println("The string is lexicographically greater than the other string");
+		}
 	}
-
 }
